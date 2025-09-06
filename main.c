f9fce94efe9ff5c068de88aea5783c5d6dbc7407
Niklas Ellegaard <55400106+lessthan-3@users.noreply.github.com>
2025-09-05 17:54:29 -0700

    Power test 2

--- main.c
+++ main.c
@@ -485,7 +485,7 @@ void motor_control_loop(void) {
         char buf[9];
 
 
-        snprintf(buf, 9, "%2u>%2u", inside_count/2, pot_setting);
+        snprintf(buf, 9, "%2u>%2u", print_pressure, pot_setting);
         lcd_print(buf);
 
         // if(pot_setting > 800){
